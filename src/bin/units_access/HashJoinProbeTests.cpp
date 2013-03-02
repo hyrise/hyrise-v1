4b593bf1f03f2623310c2efe07a29af9e22fdf3e
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-03-02 08:56:09 -0800

    added copyright notice to HashBuildTests.cpp and HashJoinProbeTests.cpp

--- src/bin/units_access/HashJoinProbeTests.cpp
+++ src/bin/units_access/HashJoinProbeTests.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2012 Hasso-Plattner-Institut fuer Softwaresystemtechnik GmbH. All rights reserved.
 #include "access/HashJoinProbe.h"
 #include "access/HashBuild.h"
 #include "io/shortcuts.h"
@@ -10,30 +11,30 @@ namespace access {
 class HashJoinProbeTests : public AccessTest {};
 
 TEST_F(HashJoinProbeTests, DISABLED_basic_hash_join_probe_test) {
-        const std::string header_left("A|B|C\nINTEGER|STRING|FLOAT\n0_R|0_R|0_R");
-        const std::string header_right("D|E|F\nINTEGER|STRING|FLOAT\n0_R|0_R|0_R");
-        const std::string header_ref("A|B|C|D|E|F\nINTEGER|STRING|FLOAT|INTEGER|STRING|FLOAT\n0_R|0_R|0_R|0_R|0_R|0_R");
-        auto left      = Loader::shortcuts::loadWithStringHeader("test/tables/hash_table_test.tbl", header_left);
-        auto right     = Loader::shortcuts::loadWithStringHeader("test/tables/hash_table_test.tbl", header_right);
-        auto reference = Loader::shortcuts::loadWithStringHeader("test/reference/hash_table_test_int.tbl", header_ref);
+  const std::string header_left("A|B|C\nINTEGER|STRING|FLOAT\n0_R|0_R|0_R");
+  const std::string header_right("D|E|F\nINTEGER|STRING|FLOAT\n0_R|0_R|0_R");
+  const std::string header_ref("A|B|C|D|E|F\nINTEGER|STRING|FLOAT|INTEGER|STRING|FLOAT\n0_R|0_R|0_R|0_R|0_R|0_R");
+  auto left      = Loader::shortcuts::loadWithStringHeader("test/tables/hash_table_test.tbl", header_left);
+  auto right     = Loader::shortcuts::loadWithStringHeader("test/tables/hash_table_test.tbl", header_right);
+  auto reference = Loader::shortcuts::loadWithStringHeader("test/reference/hash_table_test_int.tbl", header_ref);
 
-        HashBuild hb;
-        hb.addInput(right);
-        hb.addField(0);
-        hb.setKey("join");
-        hb.execute();
+  HashBuild hb;
+  hb.addInput(right);
+  hb.addField(0);
+  hb.setKey("join");
+  hb.execute();
 
-        auto right_hash = hb.getResultHashTable();
+  auto right_hash = hb.getResultHashTable();
 
-        HashJoinProbe hjp;
-        hjp.addInput(left);
-        hjp.addField(0);
-        hjp.addInputHash(right_hash);
-        hjp.execute();
+  HashJoinProbe hjp;
+  hjp.addInput(left);
+  hjp.addField(0);
+  hjp.addInputHash(right_hash);
+  hjp.execute();
 
-        auto result = hjp.getResultTable();
+  auto result = hjp.getResultTable();
 
-        EXPECT_RELATION_EQ(result, reference);
+  EXPECT_RELATION_EQ(result, reference);
 }
 
 }
