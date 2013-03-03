8dcde2273fd7eaff0bba10060d0c691132677930
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-03-03 11:59:37 -0800

    cleaned up access/Layouter

--- src/lib/access/Layouter.cpp
+++ src/lib/access/Layouter.cpp
@@ -1,101 +1,32 @@
 // Copyright (c) 2012 Hasso-Plattner-Institut fuer Softwaresystemtechnik GmbH. All rights reserved.
-#include <layouter.h>
+#include "access/Layouter.h"
 
-#include <storage/AbstractDictionary.h>
-#include <storage/OrderIndifferentDictionary.h>
-#include <storage/Table.h>
+#include "access/QueryParser.h"
+#include "storage/OrderIndifferentDictionary.h"
+#include "storage/Table.h"
 
-#include "Layouter.h"
+namespace hyrise {
+namespace access {
 
-#include "QueryParser.h"
-
-bool LayoutSingleTable::is_registered = QueryParser::registerPlanOperation<LayoutSingleTable>();
-
-using namespace layouter;
-
-std::shared_ptr<_PlanOperation> LayoutSingleTable::parse(Json::Value &data) {
-  std::shared_ptr<LayoutSingleTable> s = std::make_shared<LayoutSingleTable>();
-
-  s->setNumRows(data["num_rows"].asUInt());
-
-  if (data.isMember("layouter")) {
-
-    if (data["layouter"] == "BaseLayouter")
-      s->setLayouter(BaseLayouter);
-    else if (data["layouter"] == "CandidateLayouter")
-      s->setLayouter(CandidateLayouter);
-    else if (data["layouter"] == "DivideAndConquerLayouter")
-      s->setLayouter(DivideAndConquerLayouter);
-    else
-      throw std::runtime_error("Layouter not available, chose different implementation");
-
-  } else {
-    s->setLayouter(BaseLayouter);
-  }
-
-
-  // Parse attributes
-  if (data["attributes"].isArray()) {
-    for (unsigned i = 0; i < data["attributes"].size(); ++i) {
-      s->addFieldName(data["attributes"][i].asString());
-    }
-  } else {
-    throw QueryParserException("Parse LayoutSingleTable failed: no attributes defined");
-  }
-  // Parse queries
-  if (data["operators"].isArray()) {
-    Json::Value ops = data["operators"];
-    for (unsigned i = 0; i < ops.size(); ++i) {
-      Json::Value q = ops[i];
-      BaseQuery query;
-      query.selectivity = q["selectivity"].asDouble();
-      query.weight = q["weight"].asUInt64();
-
-      Json::Value a = q["attributes"];
-      for (unsigned j = 0; j < a.size(); ++j) {
-        std::string name = a[j].asString();
-        // find name in _names to get the position
-        for (unsigned k = 0; k < s->_names.size(); ++k)
-          if (name.compare(s->_names[k]) == 0)
-            query.positions.push_back(k);
-      }
-
-      // Add query to layouter
-      s->addQuery(query);
-    }
-  } else {
-    throw QueryParserException("Parse LayoutSingleTable failed: no queries defined");
-  }
-
-  // TODO from JSON Parsing
-  if (data.isMember("max_results"))
-    s->setMaxResults(data["max_results"].asUInt());
-  else
-    s->setMaxResults(10);
-
-  return s;
+namespace {
+  auto _ = QueryParser::registerPlanOperation<LayoutSingleTable>("LayoutSingleTable");
 }
 
-layouter::Query *LayoutSingleTable::parseQuery(BaseQuery q) {
-  LayouterConfiguration::access_type_t t = q.selectivity == 1.0 ?
-      LayouterConfiguration::access_type_fullprojection : LayouterConfiguration::access_type_outoforder;
+LayoutSingleTable::LayoutSingleTable() : _numRows(0),
+                                         _layouter(CandidateLayouter),
+                                         _maxResults(1) {
+}
 
-  double sel = q.selectivity == 1.0 ? -1.0 : q.selectivity;
-
-  layouter::Query *q1 = new layouter::Query(t, q.positions, sel, q.weight);
-
-  return q1;
+LayoutSingleTable::~LayoutSingleTable() {
 }
 
 void LayoutSingleTable::executePlanOperation() {
-
   layouter::Schema s(_atts, _numRows, _names);
 
   std::vector<layouter::Query *> qs;
-for (const auto & q: _queries) {
-    Query *tmp = parseQuery(q);
+  for (const auto & q: _queries) {
+    layouter::Query *tmp = parseQuery(q);
     qs.push_back(tmp);
-
     s.add(tmp);
   }
 
@@ -118,11 +49,8 @@ for (const auto & q: _queries) {
   }
 
   bl->layout(s, HYRISE_COST);
-
   r = bl->getNBestResults(_maxResults);
   size = bl->count();
-
-
 
   AbstractTable::SharedTablePtr result;
   metadata_list vc;
@@ -157,19 +85,115 @@ for (const auto & q: _queries) {
     result->setValue<float>(5, i, bl->getRowCost());
   }
 
-
   // Free the memory
   delete bl;
-
-for (const auto & m: vc) {
+  for (const auto & m: vc) {
     delete m;
   }
-
-for (const auto & q: qs) {
+  for (const auto & q: qs) {
     delete q;
   }
 
   addResult(result);
 }
 
+std::shared_ptr<_PlanOperation> LayoutSingleTable::parse(Json::Value &data) {
+  std::shared_ptr<LayoutSingleTable> s = std::make_shared<LayoutSingleTable>();
+  s->setNumRows(data["num_rows"].asUInt());
 
+  if (data.isMember("layouter")) {
+    if (data["layouter"] == "BaseLayouter")
+      s->setLayouter(BaseLayouter);
+    else if (data["layouter"] == "CandidateLayouter")
+      s->setLayouter(CandidateLayouter);
+    else if (data["layouter"] == "DivideAndConquerLayouter")
+      s->setLayouter(DivideAndConquerLayouter);
+    else
+      throw std::runtime_error("Layouter not available, chose different implementation");
+  } else {
+    s->setLayouter(BaseLayouter);
+  }
+
+  // Parse attributes
+  if (data["attributes"].isArray()) {
+    for (unsigned i = 0; i < data["attributes"].size(); ++i) {
+      s->addFieldName(data["attributes"][i].asString());
+    }
+  } else {
+    throw QueryParserException("Parse LayoutSingleTable failed: no attributes defined");
+  }
+
+  // Parse queries
+  if (data["operators"].isArray()) {
+    Json::Value ops = data["operators"];
+    for (unsigned i = 0; i < ops.size(); ++i) {
+      Json::Value q = ops[i];
+      BaseQuery query;
+      query.selectivity = q["selectivity"].asDouble();
+      query.weight = q["weight"].asUInt64();
+
+      Json::Value a = q["attributes"];
+      for (unsigned j = 0; j < a.size(); ++j) {
+        std::string name = a[j].asString();
+        // find name in _names to get the position
+        for (unsigned k = 0; k < s->_names.size(); ++k)
+          if (name.compare(s->_names[k]) == 0)
+            query.positions.push_back(k);
+      }
+
+      // Add query to layouter
+      s->addQuery(query);
+    }
+  } else {
+    throw QueryParserException("Parse LayoutSingleTable failed: no queries defined");
+  }
+
+  // TODO from JSON Parsing
+  if (data.isMember("max_results"))
+    s->setMaxResults(data["max_results"].asUInt());
+  else
+    s->setMaxResults(10);
+
+  return s;
+}
+
+void LayoutSingleTable::addFieldName(const std::string n) {
+  _names.push_back(n);
+
+  // TODO suport different attribute sizes
+  _atts.push_back(4);
+}
+
+void LayoutSingleTable::addQuery(const BaseQuery q) {
+  _queries.push_back(q);
+}
+
+void LayoutSingleTable::setLayouter(const layouter_type c) {
+  _layouter = c;
+}
+
+void LayoutSingleTable::setNumRows(const size_t n) {
+  _numRows = n;
+}
+
+void LayoutSingleTable::setMaxResults(const size_t n) {
+  _maxResults = n;
+}
+
+const std::string LayoutSingleTable::vname() {
+  return "LayoutSingleTable";
+}
+
+layouter::Query *LayoutSingleTable::parseQuery(const BaseQuery q) {
+  layouter::LayouterConfiguration::access_type_t t = (q.selectivity == 1.0 ?
+      layouter::LayouterConfiguration::access_type_fullprojection :
+      layouter::LayouterConfiguration::access_type_outoforder);
+
+  double sel = q.selectivity == 1.0 ? -1.0 : q.selectivity;
+  layouter::Query *q1 = new layouter::Query(t, q.positions, sel, q.weight);
+
+  return q1;
+}
+
+}
+}
