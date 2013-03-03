cfd48762c70df6f4c7ef1c20a1699892eaef4191
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-03-03 08:12:40 -0800

    cleaned up access/InsertScan

--- src/lib/access/InsertScan.h
+++ src/lib/access/InsertScan.h
@@ -2,53 +2,26 @@
 #ifndef SRC_LIB_ACCESS_INSERTSCAN_H_
 #define SRC_LIB_ACCESS_INSERTSCAN_H_
 
-#include "PlanOperation.h"
+#include "access/PlanOperation.h"
+#include "helper/types.h"
 
-/*
-  @brief The InsertScan allows to insert a single row into the database
+namespace hyrise {
+namespace access {
 
-  The idea of this scan is to create a plan operation that allows to
-  insert a new row to the database. To achieve this we have to cope
-  with different problems.
-
-  @li Make sure that the data is persisted on disk as well
-  @li Make sure the data is correctly distributed along all container
-
-*/
 class InsertScan : public _PlanOperation {
-  AbstractTable::SharedTablePtr data;
-
- public:
-
-
-  InsertScan() : _PlanOperation() {
-    
-  }
-
+public:
+  InsertScan();
   virtual ~InsertScan();
 
-  /*
-    Sets the input data for this scan operation. This input data will
-    then be distributed over all containers.
-
-    @param data the row data - materialized
-  */
-  void setInputData(AbstractTable::SharedTablePtr c);
-
-  /*
-    Execute this planoperation.
-
-    The strategy is the following: The method fetches a all containers
-    from the table and distributes the data according to the
-    distribution of the tables.
-
-    @return the input data of the operation.
-  */
+  void setInputData(const storage::atable_ptr_t c);
   virtual void executePlanOperation();
+  const std::string vname();
 
-  const std::string vname() {
-    return "InsertScan";
-  }
+private:
+  storage::atable_ptr_t _data;
 };
-#endif  // SRC_LIB_ACCESS_INSERTSCAN_H_
 
+}
+}
+
+#endif  // SRC_LIB_ACCESS_INSERTSCAN_H_
