932eb8c409f340bd05f0a4d360571222cf670f68
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-03-26 16:45:26 -0700

    cleaned up access/SetTable

--- src/lib/access/SetTable.h
+++ src/lib/access/SetTable.h
@@ -4,18 +4,25 @@
 
 #include "access/PlanOperation.h"
 
+namespace hyrise {
+namespace access {
+
 /// Provides the ability to put a table into the StorageManager
 class SetTable : public _PlanOperation
 {
 public:
   SetTable(const std::string& name);
   virtual ~SetTable();
+
   void executePlanOperation();
-  const std::string vname();
   static std::shared_ptr<_PlanOperation> parse(Json::Value &data);
+  const std::string vname();
+
 private:
   const std::string _name;
 };
 
+}
+}
 
 #endif /* SRC_LIB_ACCESS_SETTABLE_H */
