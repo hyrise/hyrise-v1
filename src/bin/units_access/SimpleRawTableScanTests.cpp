f3e11bfc0faf9b29b66e126c36b71f23ae89b5b1
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-03-27 15:57:05 -0700

    added SimpleRawTableScanTests to units_access

---
+++ src/bin/units_access/SimpleRawTableScanTests.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2012 Hasso-Plattner-Institut fuer Softwaresystemtechnik GmbH. All rights reserved.
+#include "access/SimpleRawTableScan.h"
+#include "access/predicates.h"
+#include "helper/types.h"
+#include "io/shortcuts.h"
+#include "storage/RawTable.h"
+#include "storage/storage_types.h"
+#include "testing/test.h"
+
+namespace hyrise {
+namespace access {
+
+class SimpleRawTableScanTests : public AccessTest {
+public:
+  storage::atable_ptr_t createRawTable() {
+    metadata_vec_t columns({ *ColumnMetadata::metadataFromString("INTEGER", "col1"),
+                             *ColumnMetadata::metadataFromString("STRING", "col2"),
+                             *ColumnMetadata::metadataFromString("FLOAT", "col3") });
+    auto main = std::make_shared<RawTable<>>(columns);
+    storage::rawtable::RowHelper rh(columns);
+    unsigned char *data = nullptr;
+
+    for(size_t i=0; i<10; i++) {
+      rh.set<storage::hyrise_int_t>(0, i);
+      rh.set<storage::hyrise_string_t>(1, "SomeText" + std::to_string(i));
+      rh.set<storage::hyrise_float_t>(2, 1.1*i);
+      data = rh.build();
+      main->appendRow(data);
+      free(data);
+    }
+
+    return main;
+  }
+};
+
+TEST_F(SimpleRawTableScanTests, basic_simple_raw_table_scan_test) {
+  auto t = createRawTable();
+  auto expr = new EqualsExpressionRaw<storage::hyrise_int_t>(t, 0, 5);
+
+  SimpleRawTableScan srts(expr);
+  srts.addInput(t);
+  srts.execute();
+
+  const auto &result = srts.getResultTable();
+
+  ASSERT_EQ((unsigned) 1, result->size());
+  ASSERT_EQ((storage::hyrise_int_t) 5, result->getValue<storage::hyrise_int_t>(0, 0));
+}
+
+TEST_F(SimpleRawTableScanTests, simple_raw_table_scan_fail_on_not_raw_test) {
+  auto t = Loader::shortcuts::load("test/lin_xxs.tbl");
+  auto expr = new EqualsExpressionRaw<storage::hyrise_int_t>(t, 0, 5);
+
+  SimpleRawTableScan srts(expr);
+  srts.addInput(t);
+
+  ASSERT_THROW(srts.execute(), std::runtime_error);
+}
+
+}
+}
