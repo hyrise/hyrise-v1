20be647fb22620474ecf5afdc163a91edfc525ff
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-04-04 14:15:31 -0700

    cleaned up ExpressionScan and added tests

--- src/lib/access/ExpressionScan.h
+++ src/lib/access/ExpressionScan.h
@@ -3,52 +3,52 @@
 #define SRC_LIB_ACCESS_EXPRESSIONSCAN_H_
 
 #include "access/PlanOperation.h"
+#include "helper/types.h"
 
 namespace hyrise {
 namespace access {
 
 class ColumnExpression {
 public:
-  explicit ColumnExpression(hyrise::storage::atable_ptr_t t);
+  explicit ColumnExpression(const storage::atable_ptr_t &t);
   virtual ~ColumnExpression();
 
-  virtual void setResult(hyrise::storage::atable_ptr_t result,
-                         const field_t column,
-                         const size_t row) const = 0;
+  virtual void setResult(const storage::atable_ptr_t &result,
+                         const storage::field_t column,
+                         const storage::pos_t row) const = 0;
   virtual std::string getName() const = 0;
   virtual DataType getType() const = 0;
 
 protected:
-  hyrise::storage::atable_ptr_t _table;
+  storage::atable_ptr_t _table;
 };
 
 class AddExp : public ColumnExpression {
 public:
-  AddExp(hyrise::storage::atable_ptr_t t,
-         const field_t field1,
-         const field_t field2);
+  AddExp(const storage::atable_ptr_t &t,
+         const storage::field_t field1,
+         const storage::field_t field2);
   virtual ~AddExp();
 
-  virtual void setResult(hyrise::storage::atable_ptr_t result,
-                         const field_t column,
-                         const size_t row) const;
-  virtual std::string getName() const;
-  virtual DataType getType() const;
+  void setResult(const storage::atable_ptr_t &result,
+                 const storage::field_t column,
+                 const storage::pos_t row) const;
+  std::string getName() const;
+  DataType getType() const;
 
-protected:
-  field_t _field1;
-  field_t _field2;
+private:
+  storage::field_t _field1;
+  storage::field_t _field2;
 };
 
 class ExpressionScan : public _PlanOperation {
 public:
-  ExpressionScan();
   virtual ~ExpressionScan();
 
-  virtual void setExpression(std::string name,
-                             ColumnExpression *expression);
   virtual void executePlanOperation();
   const std::string vname();
+  virtual void setExpression(const std::string &name,
+                             ColumnExpression *expression);
 
 private:
   ColumnExpression *_expression;
