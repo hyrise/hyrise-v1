0279b053bee9f8c7ce47f4a60fabca81d97b667c
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-04-04 14:07:20 -0700

    cleaned up CreateIndex and added Tests

--- src/lib/access/CreateIndex.h
+++ src/lib/access/CreateIndex.h
@@ -7,20 +7,16 @@
 namespace hyrise {
 namespace access {
 
-/// column needs to be passed via "fields" json field
 class CreateIndex : public _PlanOperation {
 public:
-  /**
-   * parse JSON fields
-   * set index name in field "table_name"
-   * set column in field "fields"
-   */
+  virtual ~CreateIndex();
+
+  void executePlanOperation();
+  /// set index name in field "table_name"
+  /// set column in field "fields"
   static std::shared_ptr<_PlanOperation> parse(Json::Value &data);
   const std::string vname();
-  void setTableName(std::string t);
-
-protected:
-  virtual void executePlanOperation();
+  void setTableName(const std::string &t);
 
 private:
   std::string _table_name;
