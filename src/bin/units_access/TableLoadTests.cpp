b6ff309a3335b90cbae18ff159459d5ba6c3a0c9
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-04-04 16:43:43 -0700

    cleaned up TableLoad and added tests

---
+++ src/bin/units_access/TableLoadTests.cpp
@@ -0,0 +1,85 @@
+// Copyright (c) 2012 Hasso-Plattner-Institut fuer Softwaresystemtechnik GmbH. All rights reserved.
+#include "access/TableLoad.h"
+#include "io/shortcuts.h"
+#include "storage/RawTable.h"
+#include "testing/test.h"
+
+namespace hyrise {
+namespace access {
+
+class TableLoadTests : public AccessTest {};
+
+TEST_F(TableLoadTests, basic_table_load_test) {
+  auto t = Loader::shortcuts::load("test/lin_xxs.tbl");
+
+  TableLoad tl;
+  tl.setFileName("lin_xxs.tbl");
+  tl.setTableName("myTable");
+  tl.execute();
+
+  const auto &result = tl.getResultTable();
+
+  ASSERT_TRUE(result->contentEquals(t));
+}
+
+TEST_F(TableLoadTests, table_load_existing_test) {
+  auto t = Loader::shortcuts::load("test/lin_xxs.tbl");
+  auto sm = StorageManager::getInstance();
+  sm->loadTable("myTable2", t);
+
+  TableLoad tl;
+  tl.setTableName("myTable2");
+  tl.execute();
+
+  const auto &result = tl.getResultTable();
+
+  ASSERT_TRUE(result->contentEquals(t));
+}
+
+TEST_F(TableLoadTests, raw_table_load_test) {
+  TableLoad tl;
+  tl.setFileName("lin_xxs.tbl");
+  tl.setTableName("myTable3");
+  tl.setRaw(true);
+  tl.execute();
+
+  const auto &result = tl.getResultTable();
+  auto raw = std::dynamic_pointer_cast<const RawTable<>>(result);
+
+  ASSERT_NE(raw.get(), (RawTable<>*) NULL);
+}
+
+TEST_F(TableLoadTests, table_load_with_string_header_test) {
+  auto t = Loader::shortcuts::loadWithHeader("test/header/data.tbl", "test/header/header.data");
+
+  std::string header = "col1_0|col1_1|col1_2|col1_3|col1_4|col1_5|col1_6|col1_7|col1_8|col1_9\n \
+                        INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER|INTEGER\n \
+                        0_R|0_R|0_R|0_R|0_R|0_R|0_R|0_R|0_R|0_R";
+
+  TableLoad tl;
+  tl.setFileName("header/data.tbl");
+  tl.setHeaderString(header);
+  tl.setTableName("myTable4");
+  tl.execute();
+
+  const auto &result = tl.getResultTable();
+
+  ASSERT_TRUE(result->contentEquals(t));
+}
+
+TEST_F(TableLoadTests, table_load_with_header_file_test) {
+  auto t = Loader::shortcuts::loadWithHeader("test/header/data.tbl", "test/header/header.data");
+
+  TableLoad tl;
+  tl.setFileName("header/data.tbl");
+  tl.setHeaderFileName("header/header.data");
+  tl.setTableName("myTable5");
+  tl.execute();
+
+  const auto &result = tl.getResultTable();
+
+  ASSERT_TRUE(result->contentEquals(t));
+}
+
+}
+}
--- src/lib/access/TableLoad.h
+++ src/lib/access/TableLoad.h
@@ -2,66 +2,35 @@
 #ifndef SRC_LIB_ACCESS_TABLELOAD_H_
 #define SRC_LIB_ACCESS_TABLELOAD_H_
 
-#include <access/PlanOperation.h>
+#include "access/PlanOperation.h"
 
 namespace hyrise {
 namespace access {
+
 class LoadTests_simple_load_op_Test;
 class LoadTests_simple_unloadall_op_Test;
-}
-}
 
 class TableLoad : public _PlanOperation {
-  friend class hyrise::access::LoadTests_simple_load_op_Test;
-  friend class hyrise::access::LoadTests_simple_unloadall_op_Test;
- public:
-  TableLoad(): _hasDelimiter(false), _binary(false), _unsafe(false) {
-  }
+  friend class LoadTests_simple_load_op_Test;
+  friend class LoadTests_simple_unloadall_op_Test;
 
-  virtual ~TableLoad() {
-  }
+public:
+  TableLoad();
+  virtual ~TableLoad();
 
   void executePlanOperation();
-
-  void setTableName(std::string tablename) {
-    _table_name = tablename;
-  }
-  void setFileName(std::string filename) {
-    _file_name = filename;
-  }
-  void setHeaderFileName(std::string filename) {
-    _header_file_name = filename;
-  }
-  void setHeaderString(std::string header) {
-    _header_string = header;
-  }
-  void setBinary(bool binary) {
-    _binary = binary;
-  }
-  void setUnsafe(bool unsafe) {
-    _unsafe = unsafe;
-  }
-
-  void setRaw(bool raw) {
-    _raw = raw;
-  }
-
-  void setDelimiter(std::string d) {
-    _delimiter = d;
-    _hasDelimiter = true;
-  }
-
   static std::shared_ptr<_PlanOperation> parse(Json::Value &data);
-  static bool is_registered;
-  static std::string name() {
-    return "TableLoad";
-  }
+  const std::string vname();
+  void setTableName(const std::string &tablename);
+  void setFileName(const std::string &filename);
+  void setHeaderFileName(const std::string &filename);
+  void setHeaderString(const std::string &header);
+  void setBinary(const bool binary);
+  void setUnsafe(const bool unsafe);
+  void setRaw(const bool raw);
+  void setDelimiter(const std::string &d);
 
-  const std::string vname() {
-    return "TableLoad";
-  }
-
- private:
+private:
   std::string _table_name;
   std::string _header_file_name;
   std::string _file_name;
@@ -73,5 +42,7 @@ class TableLoad : public _PlanOperation {
   bool _raw;
 };
 
-#endif  // SRC_LIB_ACCESS_TABLELOAD_H_
+}
+}
 
+#endif  // SRC_LIB_ACCESS_TABLELOAD_H_
