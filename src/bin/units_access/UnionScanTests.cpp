e2e168273a93b8438e7de5cac4aac8c02b39ea25
bastih <sebastian.hillig@gmail.com>
2013-04-11 11:10:18 +0200

    Distinguish Union and UnionAll
    This moves the expected behavior from UnionScan to UnionAll.
    Union will remove duplicate positions while UnionAll will not.

--- src/bin/units_access/UnionScanTests.cpp
+++ src/bin/units_access/UnionScanTests.cpp
@@ -1,25 +1,23 @@
 // Copyright (c) 2012 Hasso-Plattner-Institut fuer Softwaresystemtechnik GmbH. All rights reserved.
 #include "access/UnionScan.h"
 #include "io/shortcuts.h"
+#include "storage/PointerCalculator.h"
 #include "testing/test.h"
 
-namespace hyrise {
-namespace access {
+namespace hyrise { namespace access {
 
-class UnionScanTests : public AccessTest {};
-
-TEST_F(UnionScanTests, basic_union_scan_test) {
+TEST(UnionScanTests, basic_union_scan_test) {
   auto t = Loader::shortcuts::load("test/lin_xxs.tbl");
+  auto pc = std::make_shared<PointerCalculator>(t, nullptr, nullptr);
 
   UnionScan us;
-  us.addInput(t);
-  us.addInput(t);
+  us.addInput(pc);
+  us.addInput(pc);
   us.execute();
 
   const auto &result = us.getResultTable();
 
-  ASSERT_EQ(2 * t->size(), result->size());
+  ASSERT_EQ(t->size(), result->size());
 }
 
-}
-}
+}}
