ba0176038bcd80bdaf2507f7a13494cc4573f00f
bastih <sebastian.hillig@gmail.com>
2013-05-13 14:42:24 +0200

    Better defaults

--- config.clang++.mk
+++ config.clang++.mk
@@ -1,6 +1,6 @@
 export CCACHE_CPP2 := yes
 
-BUILD_FLAGS := -fcolor-diagnostics -Qunused-arguments -Wno-covered-switch-default -Weverything
+BUILD_FLAGS := -fcolor-diagnostics -Qunused-arguments -Wno-covered-switch-default -Weverything -Wno-c++98-compat
 
 CXX := clang++
 CC := clang
