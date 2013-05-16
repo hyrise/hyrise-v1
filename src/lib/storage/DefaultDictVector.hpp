077cd90532c819d135b29d49f376c41928761377
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2013-05-16 14:21:14 +0200

    DefaultDictVector cleanup and added comments.

--- src/lib/storage/DefaultDictVector.hpp
+++ src/lib/storage/DefaultDictVector.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <vector>
-#include <iterator>
 #include <boost/dynamic_bitset.hpp>
 
 #include <storage/BaseAttributeVector.h>
@@ -10,9 +9,8 @@
 #include <memory/MallocStrategy.h>
 #include <helper/types.h>
 
-#include <iostream>
 
-
+// stuct storing information about one column
 template <typename T>
 struct DefaultDictColumn {
   T default_value;
@@ -29,7 +27,12 @@ enum defaultDictValues {
 };
 
 
-// use malloc to allocate memory
+// DefaultDictVector will store one default value per column and all values for rows, that do not store 
+// default values.
+//   default_bit_vector per column stores, which rows hold default/not default values.
+//   For not default values, exception_positions stores, where to find the related value in exception_values.
+// DefaultDictVector will use std allocator for data vectors, because hyrise allocator can not directly be
+// used in std::vector
 template <typename T, typename Allocator = StrategizedAllocator<T, MallocStrategy > >
 class DefaultDictVector : public BaseAllocatedAttributeVector<DefaultDictVector<T, Allocator>, Allocator> {
 private:
@@ -40,18 +43,17 @@ private:
   
 public:    
   explicit DefaultDictVector(size_t columns, size_t rows);
-  ~DefaultDictVector();
+  ~DefaultDictVector() override;
 
-  void *data() {
+  explicit DefaultDictVector(const DefaultDictVector& other);
+
+  void *data() override {
     throw std::runtime_error("Direct data access not allowed");
   }
 
-  void setNumRows(size_t rows) {
-    //_rows = rows;
+  void setNumRows(size_t rows) override {
     throw std::runtime_error("Direct data access not allowed");
   }
-  
-  size_t allocated_bytes();
 
   T default_value (size_t column) const {
     checkColumnAccess(column);
@@ -59,77 +61,32 @@ public:
   }
 
 
-  inline T get(size_t column, size_t row) const {
-    checkAccess(column, row);
-    if(_default_dict_table[column].default_bit_vector[row]) {
-      return _default_dict_table[column].default_value;
-    }
-    auto exception_position = std::lower_bound(
-      _default_dict_table[column].exception_positions.begin(),
-      _default_dict_table[column].exception_positions.end(),
-      row);
-    return _default_dict_table[column].exception_values[
-      ( exception_position - _default_dict_table[column].exception_positions.begin() )];
-  }
+  inline T get(size_t column, size_t row) const override;
 
+  inline void set(size_t column, size_t row, T value) override;
 
-  inline void set(size_t column, size_t row, T value) {
-    checkAccess(column,row);
-    auto& def_bits(_default_dict_table[column].default_bit_vector);
-    auto& ex_pos(_default_dict_table[column].exception_positions);
-    auto& ex_val(_default_dict_table[column].exception_values);
-    auto& def_val(_default_dict_table[column].default_value);
+  void reserve(size_t rows) override;
 
-    if (! _default_dict_table[column].defaultValueIsSet) {
-      _default_dict_table[column].defaultValueIsSet = true;
-      def_val = value;
-    }
-
-    if ((def_bits[row]==defaultDict_default_value) and (value == def_val))
-      return;
-
-    size_t pos = std::lower_bound(ex_pos.begin(),ex_pos.end(),row) - ex_pos.begin();
-
-    //value is not default value but old value was
-    if (def_bits[row]) {
-      def_bits[row] = defaultDict_non_default_value;
-      auto ex_it = ex_pos.begin();
-      ex_pos.insert(ex_it+pos,row);
-      auto val_it = ex_val.begin();
-      ex_val.insert(val_it+pos,value);
-    } else {
-      if (value == def_val) {
-        def_bits[row] = defaultDict_default_value;
-        ex_pos.erase(ex_pos.begin()+pos);
-        ex_val.erase(ex_val.begin()+pos);
-      } else {
-        ex_val[pos] = value;
-      }
-    }
-  }
-
-  void reserve(size_t rows);
-
-  void resize(size_t rows) {
+  void resize(size_t rows) override {
     reserve(rows);
     _rows = rows;
   }
 
-  inline uint64_t capacity() {
+  inline uint64_t capacity() override {
     if (!_columns)
       return 0;
     return _default_dict_table[0].default_bit_vector.size();
   }
 
-  void clear();
+  void clear() override;
 
-  size_t size() {
+  size_t size() override {
     return _rows;
   }
 
   std::shared_ptr<BaseAttributeVector<T>> copy();
 
-  void rewriteColumn(const size_t column, const size_t bits) { }
+  void rewriteColumn(const size_t column, const size_t bits) override { }
 
 
 private:
@@ -162,6 +119,8 @@ DefaultDictVector<T, Allocator>::DefaultDictVector(size_t columns, size_t rows) 
   _columns(columns),
   _rows(0) {
 
+  // create data structure for each column
+  // default values are not set here, they will be set on first write per column
   DefaultDictColumn<T> column;
   for (size_t c = 0; c<_columns; ++c) {
     column.defaultValueIsSet = false;
@@ -175,14 +134,23 @@ template <typename T, typename Allocator>
 DefaultDictVector<T, Allocator>::~DefaultDictVector() {
 }
 
+
+template <typename T, typename Allocator>
+DefaultDictVector<T, Allocator>::DefaultDictVector(const DefaultDictVector& other) : 
+  _default_dict_table(other._default_dict_table),
+  _columns(other._columns),
+  _rows(other._rows)
+{
+}
+
 template <typename T, typename Allocator>
 void DefaultDictVector<T, Allocator>::clear() {
-  for(size_t c = 0; c<_columns; ++c) {
-    _default_dict_table[c].defaultValueIsSet = false;
-    _default_dict_table[c].default_value = T(0);
-    _default_dict_table[c].default_bit_vector.clear();
-    _default_dict_table[c].exception_positions.clear();
-    _default_dict_table[c].exception_values.clear();
+  for (DefaultDictColumn<T>& column: _default_dict_table) {
+    column.defaultValueIsSet = false;
+    column.default_value = T(0);
+    column.default_bit_vector.clear();
+    column.exception_positions.clear();
+    column.exception_values.clear();
   }
 
   _rows = 0;
@@ -190,16 +158,77 @@ void DefaultDictVector<T, Allocator>::clear() {
 
 template <typename T, typename Allocator>
 std::shared_ptr<BaseAttributeVector<T>> DefaultDictVector<T, Allocator>::copy() {
-  throw std::bad_exception();
-  //auto copy = std::make_shared<DefaultDictVector<T, Allocator>>(_default_value);
-  //copy->_rows = _rows;
+  DefaultDictVector<T, Allocator> *copy = new DefaultDictVector<T, Allocator>(*this);
+  return std::shared_ptr<BaseAttributeVector<T>>(copy);
 }
 
 template <typename T, typename Allocator>
 void DefaultDictVector<T, Allocator>::reserve(size_t rows) {
+  // reserve will never shrink the size of the Vector
   if (_default_dict_table[0].default_bit_vector.size() >= rows)
     return;
 
-  for (size_t c=0; c<_columns; ++c)
-    _default_dict_table[c].default_bit_vector.resize(rows, true);
+  for (DefaultDictColumn<T>& column: _default_dict_table)
+    column.default_bit_vector.resize(rows, true);
+}
+
+template <typename T, typename Allocator>
+inline T DefaultDictVector<T, Allocator>::get(size_t column, size_t row) const {
+  checkAccess(column, row);
+  // first check if requested field stores the default value
+  if(_default_dict_table[column].default_bit_vector[row] == defaultDict_default_value) {
+    return _default_dict_table[column].default_value;
+  }
+  // if not, search the specified row in the exceptions list
+  auto exception_position = std::lower_bound(
+    _default_dict_table[column].exception_positions.begin(),
+    _default_dict_table[column].exception_positions.end(),
+    row);
+  // get the exception value at determined position
+  return _default_dict_table[column].exception_values[
+    ( exception_position - _default_dict_table[column].exception_positions.begin() )];
+}
+
+template <typename T, typename Allocator>
+inline void DefaultDictVector<T, Allocator>::set(size_t column, size_t row, T value) {
+  checkAccess(column,row);
+  // create some handy shortcuts for current column
+  auto& def_bits(_default_dict_table[column].default_bit_vector);
+  auto& ex_pos(_default_dict_table[column].exception_positions);
+  auto& ex_val(_default_dict_table[column].exception_values);
+  T& def_val(_default_dict_table[column].default_value);
+
+  // the first call of set on a column will define its default value
+  if (! _default_dict_table[column].defaultValueIsSet) {
+    _default_dict_table[column].defaultValueIsSet = true;
+    def_val = value;
+  }
+
+  // nothing to do, if new and old value are default values
+  if ((def_bits[row] == defaultDict_default_value) && (value == def_val))
+    return;
+
+  // get position in exception list
+  // what to do at 'pos' depends whether old or new value is default value
+  size_t pos = std::lower_bound(ex_pos.begin(),ex_pos.end(),row) - ex_pos.begin();
+
+  if (def_bits[row] == defaultDict_default_value) {
+    // new value is not default value but old value was
+    // so insert a new exception pos/value
+    def_bits[row] = defaultDict_non_default_value;
+    auto ex_it = ex_pos.begin();
+    ex_pos.insert(ex_it+pos,row);
+    auto val_it = ex_val.begin();
+    ex_val.insert(val_it+pos,value);
+  } else {
+    // old value was not default
+    if (value == def_val) {
+      // delete exception entry, if new value is default
+      def_bits[row] = defaultDict_default_value;
+      ex_pos.erase(ex_pos.begin()+pos);
+      ex_val.erase(ex_val.begin()+pos);
+    } else {
+      ex_val[pos] = value;
+    }
+  }
 }
