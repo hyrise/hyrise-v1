b1a038b70f6fe18f36b854e7b27f6db4e62a0c44
JWUST <Johannes.Wust@hpi.uni-potsdam.de>
2013-07-29 16:09:41 +0200

    added ThreadPerTaskScheduler

---
+++ src/lib/taskscheduler/ThreadPerTaskScheduler.h
@@ -0,0 +1,65 @@
+/*
+ * ThreadPerTaskScheduler.h
+ *
+ *  Created on: Jul 22, 2013
+ *      Author: jwust
+ */
+
+#ifndef THREADPERTASKSCHEDULER_H_
+#define THREADPERTASKSCHEDULER_H_
+
+#include "AbstractTaskScheduler.h"
+#include <taskscheduler/SharedScheduler.h>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <queue>
+
+
+// our worker thread objects
+class TaskExecutor {
+  std::shared_ptr<Task> _task;
+public:
+  TaskExecutor(std::shared_ptr<Task> task) : _task(task) { }
+  void operator()(){
+    (*_task)();
+    _task->notifyDoneObservers();
+  };
+};
+
+class ThreadPerTaskScheduler : public AbstractTaskScheduler, public TaskReadyObserver {
+  typedef std::unordered_set<std::shared_ptr<Task> > waiting_tasks_t;
+    // set for tasks with open dependencies
+    waiting_tasks_t _waitSet;
+    // mutex to protect waitset
+    std::mutex _setMutex;
+    // scheduler status
+    scheduler_status_t _status;
+    // mutex to protect status
+    std::mutex _statusMutex;
+
+    static log4cxx::LoggerPtr _logger;
+public:
+  ThreadPerTaskScheduler();
+  ThreadPerTaskScheduler(int i);
+  virtual ~ThreadPerTaskScheduler();
+  /*
+   * schedule a task for execution
+   */
+  virtual void schedule(std::shared_ptr<Task> task);
+  /*
+   * shutdown task scheduler; makes sure all underlying threads are stopped
+   */
+  virtual void shutdown();
+
+  /**
+   * get number of worker
+   */
+  virtual size_t getNumberOfWorker() const {
+      return 0;
+  };
+
+  void notifyReady(std::shared_ptr<Task> task);
+};
+
+#endif /* THREADPERTASKSCHEDULER_H_ */
