2ebacad2119830c9e7a6a8441a8e214947d760b0
lordgeorg <lordgeorg@googlemail.com>
2013-10-17 14:11:04 +0200

    [WIP] adds JoinScan::addClause() with field name

--- src/bin/units_access/JoinScanTests.cpp
+++ src/bin/units_access/JoinScanTests.cpp
@@ -9,7 +9,7 @@ namespace access {
 
 class JoinScanTests : public AccessTest {};
 
-TEST_F(JoinScanTests, basic_join_scan_test) {
+TEST_F(JoinScanTests, basic_join_scan_by_fieldId_test) {
   auto t1 = Loader::shortcuts::load("test/join_transactions.tbl");
   auto t2 = Loader::shortcuts::load("test/join_exchange.tbl");
   auto reference = Loader::shortcuts::load("test/reference/join_result.tbl");
@@ -20,6 +20,24 @@ TEST_F(JoinScanTests, basic_join_scan_test) {
   js.addCombiningClause(AND);
   js.addJoinClause<int>(0,0,1,0);
   js.addJoinClause<std::string>(0,1,1,1);
+  js.execute();
+
+  const auto &result = js.getResultTable();
+
+  ASSERT_TABLE_EQUAL(result, reference);
+}
+
+TEST_F(JoinScanTests, basic_join_scan_by_fieldName_test) {
+  auto t1 = Loader::shortcuts::load("test/join_transactions.tbl");
+  auto t2 = Loader::shortcuts::load("test/join_exchange.tbl");
+  auto reference = Loader::shortcuts::load("test/reference/join_result.tbl");
+
+  JoinScan js(JoinType::EQUI);
+  js.addInput(t1);
+  js.addInput(t2);
+  js.addCombiningClause(AND);
+  js.addJoinClause<int>(0,"year",1,"year");
+  js.addJoinClause<std::string>(0,"currency",1,"source_currency");
   js.execute();
 
   const auto &result = js.getResultTable();
