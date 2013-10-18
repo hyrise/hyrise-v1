c266d45131b054d4ab49d0d941463ff318e8a8ce
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2013-10-18 15:06:50 +0200

    Add TPCCH Query 1
    includes fixed tpcc test tables
    runs query1 in test suite

--- src/bin/units_access/TpccTableLoadTest.cpp
+++ src/bin/units_access/TpccTableLoadTest.cpp
@@ -10,8 +10,8 @@ namespace access {
 class TpccTableLoadTests : public AccessTest {};
 uint tableLoad(std::string tableName){
     TableLoad tl;
-    tl.setFileName("tpcc/"+tableName+".csv");
-    tl.setDelimiter(",");
+    tl.setFileName("tpcc/"+tableName+".tbl");
+    tl.setDelimiter("|");
     tl.setHeaderFileName("tpcc/"+tableName+"_header.tbl");
     tl.setTableName("tpcc_"+tableName);
     tl.execute();
@@ -20,47 +20,47 @@ uint tableLoad(std::string tableName){
     return result->size();
 }
 
-// Table: customer Lines in CSV: 10 (load from test/tpcc/ customer.csv, customer_header.tbl)
+// Table: customer Lines in CSV: 10 (load from test/tpcc/ customer.tbl, customer_header.tbl)
 TEST_F(TpccTableLoadTests, table_load_with_header_file_test_customer) {
   ASSERT_EQ(10u,tableLoad("customer"));
 }
 
-// Table: district Lines in CSV: 5 (load from test/tpcc/ district.csv, district_header.tbl)
+// Table: district Lines in CSV: 5 (load from test/tpcc/ district.tbl, district_header.tbl)
 TEST_F(TpccTableLoadTests, table_load_with_header_file_test_district) {
   ASSERT_EQ(5u,tableLoad("district"));
 }
 
-// Table: history Lines in CSV: 10 (load from test/tpcc/ history.csv, history_header.tbl)
+// Table: history Lines in CSV: 10 (load from test/tpcc/ history.tbl, history_header.tbl)
 TEST_F(TpccTableLoadTests, table_load_with_header_file_test_history) {
   ASSERT_EQ(10u,tableLoad("history"));
 }
 
-// Table: item Lines in CSV: 10 (load from test/tpcc/ item.csv, item_header.tbl)
+// Table: item Lines in CSV: 10 (load from test/tpcc/ item.tbl, item_header.tbl)
 TEST_F(TpccTableLoadTests, table_load_with_header_file_test_item) {
   ASSERT_EQ(10u,tableLoad("item"));
 }
 
-// Table: new_order Lines in CSV: 10 (load from test/tpcc/ new_order.csv, new_order_header.tbl)
+// Table: new_order Lines in CSV: 10 (load from test/tpcc/ new_order.tbl, new_order_header.tbl)
 TEST_F(TpccTableLoadTests, table_load_with_header_file_test_new_order) {
   ASSERT_EQ(10u,tableLoad("new_order"));
 }
 
-// Table: order_line Lines in CSV: 10 (load from test/tpcc/ order_line.csv, order_line_header.tbl)
+// Table: order_line Lines in CSV: 10 (load from test/tpcc/ order_line.tbl, order_line_header.tbl)
 TEST_F(TpccTableLoadTests, table_load_with_header_file_test_order_line) {
   ASSERT_EQ(10u,tableLoad("order_line"));
 }
 
-// Table: orders Lines in CSV: 10 (load from test/tpcc/ orders.csv, orders_header.tbl)
+// Table: orders Lines in CSV: 10 (load from test/tpcc/ orders.tbl, orders_header.tbl)
 TEST_F(TpccTableLoadTests, table_load_with_header_file_test_orders) {
   ASSERT_EQ(10u,tableLoad("orders"));
 }
 
-// Table: stock Lines in CSV: 10 (load from test/tpcc/ stock.csv, stock_header.tbl)
+// Table: stock Lines in CSV: 10 (load from test/tpcc/ stock.tbl, stock_header.tbl)
 TEST_F(TpccTableLoadTests, table_load_with_header_file_test_stock) {
   ASSERT_EQ(10u,tableLoad("stock"));
 }
 
-// Table: warehouse Lines in CSV: 1 (load from test/tpcc/ warehouse.csv, warehouse_header.tbl)
+// Table: warehouse Lines in CSV: 1 (load from test/tpcc/ warehouse.tbl, warehouse_header.tbl)
 TEST_F(TpccTableLoadTests, table_load_with_header_file_test_warehouse) {
   ASSERT_EQ(1u,tableLoad("warehouse"));
 }
---
+++ src/bin/units_access/TpcchQueryTest.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2012 Hasso-Plattner-Institut fuer Softwaresystemtechnik GmbH. All rights reserved.
+#include "testing/test.h"
+#include "io/StorageManager.h"
+
+#include "helper.h"
+
+namespace hyrise {
+namespace access {
+
+class TpcchQueryTest : public AccessTest {};
+TEST_F(TpcchQueryTest, analyt_query1) {
+  StorageManager * sm = StorageManager::getInstance();
+
+  sm->loadTableFileWithHeader("order_line", "tpcc/order_line.tbl", "tpcc/order_line_header.tbl");
+  sm->loadTableFile("expectedTable", "tpcch/query1_result.tbl");
+
+  std::string query = loadFromFile("test/tpcch/query1.json");
+
+  const auto& out = executeAndWait(query);
+
+  ASSERT_TRUE(out != nullptr);
+
+  ASSERT_TABLE_EQUAL(out, sm->getTable("expectedTable"));
+}
+
+}
+}
