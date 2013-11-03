a8a5cbd3b07a2d68bc310c37e70372dbcefc43e6
lordgeorg <lordgeorg@googlemail.com>
2013-11-03 15:11:37 +0100

    better newOrder python script

--- tools/newOrder.py
+++ tools/newOrder.py
@@ -1,5 +1,5 @@
 #!/bin/env python2
 from client import Connection
 c = Connection()
-response = c.stored_procedure("TPCC-NewOrder", """{ "W_ID": 1, "D_ID": 1, "C_ID": 1, "O_CARRIER_ID": 5, "OL_DIST_INFO": "lalala", "items": [{1, 1, 5}] }""")
+response = c.stored_procedure("TPCC-NewOrder", """{ "W_ID": 1, "D_ID": 1, "C_ID": 1, "O_CARRIER_ID": 5, "OL_DIST_INFO": "lalala", "items": [{"I_ID":1, "I_W_ID": 1, "quantity": 1}]}""")
 print response
