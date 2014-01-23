94739333794764616666ab622cb544d8026e87e4
bastih <sebastian.hillig@gmail.com>
2014-01-23 14:02:57 +0000

    Add reformat check + allow optional clang 3.4 strategy

--- makefiles/reformat.mk
+++ makefiles/reformat.mk
@@ -1,2 +1,4 @@
 reformat: $(PROJECT_ROOT)/tools/clang34
 	$(PROJECT_ROOT)/tools/clang34/bin/clang-format -i -style='{BasedOnStyle: Google, BinPackParameters: false, AlignTrailingComments: false, DerivePointerBinding: false, PointerBindsToType: true, MaxEmptyLinesToKeep: 3, ColumnLimit: 120, AllowAllParametersOfDeclarationOnNextLine: false, AllowShortLoopsOnASingleLine: false, AllowShortIfStatementsOnASingleLine: false, ConstructorInitializerAllOnOneLineOrOnePerLine: true}'  `find src/ -type f -regextype posix-awk -regex "(.*\.(h|cpp|cc|hpp))"` 
+reformat-check: reformat
+	if [ "`git status --porcelain | wc -l`" = "0" ]; then true; else false; fi
