e733476f85c399eaa9f8cda97bd0d4b430bf2164
bastih <sebastian.hillig@gmail.com>
2014-02-10 11:27:17 +0100

    Remove unused position intersection

--- src/lib/storage/storage_types_helper.cpp
+++ src/lib/storage/storage_types_helper.cpp
@@ -36,22 +36,4 @@ std::string data_type_to_string(DataType d) {
   }
 }
 
-pos_list_t *pos_list_intersection(pos_list_t *p1, pos_list_t *p2, const bool deleteInput) {
-  size_t max_size = p1->size() < p2->size() ? p1->size() : p2->size();
-
-  pos_list_t *result = new pos_list_t(max_size);
-
-  pos_list_t::iterator it = std::set_intersection(p1->begin(), p1->end(), p2->begin(), p2->end(), result->begin());
-
-  result->erase(it, result->end());
-
-  if (deleteInput) {
-    delete p1;
-    delete p2;
-  }
-
-  return result;
-}
-
 } // namespace hyrise
-
