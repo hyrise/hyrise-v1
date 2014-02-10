1cc07a4ba0c7ac358d9a5ddcea0e5704f3ce3d1d
bastih <sebastian.hillig@gmail.com>
2014-02-10 10:24:07 +0000

    STL-ified ColumnLoader

--- src/lib/io/ColumnLoader.h
+++ src/lib/io/ColumnLoader.h
@@ -1,7 +1,11 @@
 // Copyright (c) 2012 Hasso-Plattner-Institut fuer Softwaresystemtechnik GmbH. All rights reserved.
 #pragma once
 
+#include <algorithm>
+#include <vector>
+
 #include "storage/AbstractTable.h"
+#include "storage/BaseDictionary.h"
 
 namespace hyrise {
 namespace io {
@@ -15,56 +19,37 @@ template <typename T>
 class ColumnLoader : public AbstractColumnLoader {
  private:
   std::vector<T>  _values;
-
  public:
   explicit ColumnLoader(size_t size = 0) {
     _values.reserve(size);
   }
 
-  virtual ~ColumnLoader() {
-  }
-
-  void push_back(T value) {
+  void push_back(T&& value) {
     _values.push_back(value);
   }
 
   void write_to_table(storage::AbstractTable *table, size_t column) {
-
     if (_values.size() == 0) {
       return;
     }
-
-
-    auto *dict = (storage::BaseDictionary<T> *)table->dictionaryAt(column);
-
-    if (dict->isOrdered()) {
+    auto dict = static_cast<storage::BaseDictionary<T>*>(table->dictionaryAt(column));
+    if (dict.isOrdered()) {
       // Create the sorted list for the input
-      std::vector<T> *vec_sorted = new std::vector<T>(_values.begin(), _values.end());
-      sort(vec_sorted->begin(), vec_sorted->end());
-
-      T last_value = vec_sorted->at(0);
-      dict->addValue(last_value);
-
-      for (const auto& value: *vec_sorted) {
-        if (value == last_value) {
-          continue;
-        }
-
-        dict->addValue(value);
-        last_value = value;
+      std::vector<T> vec_sorted(_values.begin, _values.end);
+      std::sort(vec_sorted.begin(), vec_sorted.end());
+      std::unique(vec_sorted.begin(), vec_sorted.end());
+      for (const auto& value: vec_sorted) {
+        dict.addValue(value);
       }
       size_t row = 0;
       for (const auto& value: _values) {
         table->setValueId(column, row++, table->getValueIdForValue<T>(column, value));
       }
-
-      delete vec_sorted;
     } else {
       size_t row = 0;
       for (const auto& value: _values) {
         table->setValue<T>(column, row++, value);
       }
-
     }
   }
 };
