6ed1d5672ee1e0fe2b126277a39ab6745294a391
lanice <leander.neiss@student.hpi.uni-potsdam.de>
2014-04-03 11:52:47 +0000

    Add custom cereal archive for hyrise
    * use develop branch of cereal to support name-value pairs for loading
    * add Json::Value and std::optional as serializable types (load only)
    * add a serialization macro

---
+++ src/lib/helper/cereal/cereal_type_optional.h
@@ -0,0 +1,21 @@
+#ifndef CEREAL_TYPES_OPTIONAL_H_
+#define CEREAL_TYPES_OPTIONAL_H_
+
+#include <optional/optional.hpp>
+
+namespace cereal
+{
+  //! Loading std::optional
+  template <class Archive, class T> inline
+  void load( Archive & ar, std::optional<T> & opt )
+  {
+    ar.setOptional(true);
+    T optValue;
+    ar( optValue );
+    if (ar.foundOptionalValue()) opt = std::optional<T>(optValue);
+    ar.setOptional(false);
+  }
+} // namespace cereal
+
+#endif // CEREAL_TYPES_OPTIONAL_H_
+
