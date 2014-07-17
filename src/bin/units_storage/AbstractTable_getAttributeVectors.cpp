8e1eb7309d86b109a9051dd911b82b14f8d1df41
bastih <sebastian.hillig@gmail.com>
2014-07-17 12:22:28 +0000

    stand

--- src/bin/units_storage/AbstractTable_getAttributeVectors.cpp
+++ src/bin/units_storage/AbstractTable_getAttributeVectors.cpp
@@ -15,12 +15,7 @@ TEST(AbstractTable_getAttributeVectors, test) {
 TEST(AbstractTable_getAttributeVectors, test2) {
   const auto& table = io::Loader::shortcuts::load("test/tables/employees.tbl");
   const auto& vectors = table->allParts();
-  std::cout << std::endl;
-  for (auto& p : vectors) {
-    std::cout << p.column << " " << p.row << " " << p.table->size() << std::endl;
-  }
-  std::cout << std::endl;
-  // EXPECT_EQ(2u, vectors.size());
+  EXPECT_EQ(2u, vectors.size());
 }
 }
 }  // namespace hyrise::storage
