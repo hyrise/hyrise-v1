57e513e8efd004ae56e690d23d643753df69219e
bastih <sebastian.hillig@gmail.com>
2014-07-23 10:43:08 +0200

    Add installreformathook

--- src/lib/helper/checked_cast.h
+++ src/lib/helper/checked_cast.h
@@ -14,6 +14,7 @@ inline std::shared_ptr<T> checked_pointer_cast(const std::shared_ptr<U>& sp) {
     std::string s1(typeid(*sp).name());
     std::string s2(typeid(T).name());
     std::stringstream ss;
+
     ss << "checked_pointer_cast failed: Pointer is not of requested type: Tried to convert " << demangle(s1.c_str())
        << " (" << s1 << ") to " << demangle(s2.c_str()) << " (" << s2 << ")";
     throw std::runtime_error(ss.str());
