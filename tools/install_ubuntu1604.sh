fb2aea02477cc0d0c76bd56d1394f7194ea996d3
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2017-01-11 18:25:50 +0100

    Make sure libcsv is compiled with g++ (discovered by @felixeberhardt)

--- tools/install_ubuntu1604.sh
+++ tools/install_ubuntu1604.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get -y install build-essential wget bzip2 ccache cmake git liblog4cxx10 liblog4cxx10-dev libmysqlclient-dev libunwind8-dev libev-dev libtbb-dev libboost-all-dev libhwloc-dev binutils-dev libgoogle-perftools-dev  gfortran curl git
+apt-get -y install build-essential wget bzip2 ccache cmake git liblog4cxx10v5 liblog4cxx10-dev libmysqlclient-dev libunwind8-dev libev-dev libtbb-dev libboost-all-dev libhwloc-dev binutils-dev libgoogle-perftools-dev  gfortran curl git
 
 TMPDIR=/tmp/.tmp-$$
 
@@ -48,3 +48,4 @@ apt-get -y install mysql-server
 mysql -uroot -psomerootpass -e "CREATE USER 'vagrant'@'localhost' IDENTIFIED BY 'vagrant';"
 mysql -uroot -psomerootpass -e "GRANT ALL PRIVILEGES ON *.* TO 'vagrant'@'localhost';"
 mysql -uroot -psomerootpass -e "FLUSH PRIVILEGES;"
+
