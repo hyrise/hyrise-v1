afd1a73193cf5569154f98456f7c19166573e338
Felix Eberhardt <felix.eberhardt@hpi.de>
2017-01-12 16:02:22 +0100

    Add support for GCC 5

--- makefiles/config.autog++.mk
+++ makefiles/config.autog++.mk
@@ -1,12 +1,11 @@
-available := $(shell which g++-4.7 g++-4.8)
-ifeq (available,)
-$(error No suitable g++ (either 4.7 or 4.8) found. Explicitly select your compiler in settings.mk COMPILER setting)
-endif
+available := $(shell which g++-4.7 g++-4.8 g++-5)
 
-ifneq ($(findstring g++-4.8,$(available)),)
+ifneq ($(findstring g++-5,$(available)),)
+include makefiles/config.g++5.mk
+else ifneq ($(findstring g++-4.8,$(available)),)
 include makefiles/config.g++48.mk
-else
-ifneq ($(findstring g++-4.7,$(available)),)
+else ifneq ($(findstring g++-4.7,$(available)),)
 include makefiles/config.g++47.mk
-endif
+else
+$(error No suitable g++ (either 4.7, 4.8 or 5) found. Explicitly select your compiler in settings.mk COMPILER setting)
 endif
